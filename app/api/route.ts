7742993668db9e6c8498af7655da17bcded0e36d
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2026-04-05 18:48:40 -0300

    Rota e Var

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,6 +4,7 @@ export async function GET() {
   return NextResponse.json({
     nome: "Seu Nome Completo",
     email: "seu.email@exemplo.com",
-    RA: "12345678"
+    RA: "12345678",
+    variavelTeste: process.env.MEU_NOME || "Variável não definida"
   });
 }
